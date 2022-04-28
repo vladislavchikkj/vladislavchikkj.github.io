ba6981c8fcc3d0b260c2bdd9a8710d53f7848236
Vlad <vlad.kosevich@mail.ru>
2022-04-28 15:16:37 +0300

    feat: added new version of site

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/ar_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/ar_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});

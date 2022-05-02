303ce214df17c62f996de26bd062e7a2efe7e09a
Vlad <vlad.kosevich@mail.ru>
2022-05-02 15:35:29 +0300

    Last change

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,35 @@ $(document).ready(function(){
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/ar_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="src/icons/ar_right.png"></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    dots: false,
                     arrows: false
                 }
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });

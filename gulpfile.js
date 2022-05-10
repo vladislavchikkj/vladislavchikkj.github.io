2b8619468065f6a0d66e50b5afedc96fbbb02fec
Vlad <vlad.kosevich@mail.ru>
2022-05-10 23:01:20 +0300

    Finish

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
